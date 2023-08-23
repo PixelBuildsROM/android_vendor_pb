yashsonawane/platform_vendor_lucid

vendor: Add fonts for customization and set Google Sans as default font.
Signed-off-by: Arseniy Graur <argraur@aosip.dev>

configs/common.mk
@@ -36,6 +36,9 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# Fonts
+include vendor/lucid/configs/fonts.mk
+
 # Common overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/lucid/overlay
 

PixelBuildsROM/android_vendor_pb

Change-Id: Iac080faf905985c38bdb15caaa90cdc6ec8d909c

config/common.mk
@@ -173,3 +173,6 @@ include vendor/pb/config/audio.mk
 
 # Bootanimation
 include vendor/pb/config/bootanimation.mk
+
+# Fonts
+include vendor/pb/config/fonts.mk