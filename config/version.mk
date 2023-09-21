453b2238cdffd727be6eb4e90f2ee42b23dfdb2e
Beru Krugermeier <windowz414@gnuweeb.org>
2023-09-21 10:56:21 +0000

    vendor: version: Change state of version variables
    * So that it can be overriden in, for example, vendor/extra.
    
    Signed-off-by: Beru Krugermeier <windowz414@gnuweeb.org>

--- config/version.mk
+++ config/version.mk
@@ -5,10 +5,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 endif
 
 # Internal version
-PB_VERSION := trinity-alpha
+PB_VERSION ?= trinity-alpha
 
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(PB_BUILD))
-PIXELBUILDS_VERSION := PixelBuilds_$(TARGET_PRODUCT_SHORT)-$(PB_VERSION)-$(shell date +%Y%m%d-%H%M)
+PIXELBUILDS_VERSION ?= PixelBuilds_$(TARGET_PRODUCT_SHORT)-$(PB_VERSION)-$(shell date +%Y%m%d-%H%M)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.custom=$(PB_VERSION) \
