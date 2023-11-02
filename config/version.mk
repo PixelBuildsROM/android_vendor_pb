f4eb0ca340c655ac9f2351ab1efaf94e5510ac86
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2023-11-02 14:16:42 +0200

    vendor: config: version: add the build type definition
    * Also get rid of unused modversion prop
    * Set PB_BUID_TYPE var to the default overrideable value

--- config/version.mk
+++ config/version.mk
@@ -6,6 +6,7 @@ endif
 
 # Internal version
 PB_VERSION ?= unity-beta
+PB_BUID_TYPE ?= unsigned
 
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(PB_BUILD))
 PIXELBUILDS_VERSION ?= PixelBuilds_$(TARGET_PRODUCT_SHORT)-$(PB_VERSION)-$(shell date +%Y%m%d-%H%M)
@@ -14,4 +15,4 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.custom=$(PB_VERSION) \
     ro.build.version.device=$(TARGET_PRODUCT_SHORT) \
     ro.pb.build.version=$(PIXELBUILDS_VERSION) \
-    ro.modversion=$(PB_VERSION)
+    ro.pb.buildtype=$(PB_BUID_TYPE)
