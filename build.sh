318b66cb4484d5f2b699efe3cf0c53eb4d16ae5d
ExactExampl <xmpl@dontsleep.work>
2026-03-08 14:20:53 +0200

    vendor: build.sh: use fixed number of jobs for sync
    * With aggressive rate limiting from android.googlesource.com it doesn't make sense to set sync jobs count to something higher anymore, so let's stick with sane defaults
    
    Change-Id: I55e28570ed8707b7cb3271becf060db472b0b2e3

--- build.sh
+++ build.sh
@@ -137,7 +137,7 @@ fi
 if [ "$FLAG_SYNC" = 'y' ]; then
         echo -e "${CLR_BLD_BLU}Downloading the latest source files${CLR_RST}"
         echo -e ""
-        repo sync -j"$JOBS" -c --force-sync --no-clone-bundle --current-branch --no-tags 
+        repo sync -j6 -c --force-sync --no-clone-bundle --current-branch --no-tags 
 fi
 
 # Perform repopick, if topic was specified
